5ca02b7749d0a68fb223a6b9e26534d4bf1543f3
Semana_7 <koutaroox0@gmail.com>
2026-06-02 13:20:18 -0500

    Agregar archivos de proyecto.

---
+++ Grafos/Grafo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grafos
+{
+    internal class Grafo
+    {
+        private int[,] matriz;
+        private int n;
+        private Random rand = new Random();
+
+
+        public Grafo(int vertices)
+        {
+            n = vertices;
+            matriz = new int[n, n];
+
+
+            for (int f = 0; f < n; f++)
+                for (int c = 0; c < n; c++)
+                    matriz[f, c] = 0;
+        }
+
+
+        public void Agregar(int origen, int destino)
+        {
+            if (origen >= 0 && origen < n && destino >= 0 && destino < n)
+            {
+                matriz[origen, destino] = 1;
+                matriz[destino, origen] = 1;
+            }
+            else
+            {
+                Console.WriteLine("Error: indice fuera de rango");
+            }
+        }
+
+
+        public void Mostrar()
+        {
+            Console.WriteLine("Matriz de Adyacencia:");
+
+            for (int f = 0; f < n; f++)
+            {
+                for (int c = 0; c < n; c++)
+                {
+                    Console.Write(matriz[f, c] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Grafos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Grafos;
+
+Grafo g = new Grafo(4);
+
+g.Agregar(0, 1);
+g.Agregar(0, 2);
+g.Agregar(1, 3);
+g.Agregar(2, 3);
+
+g.Mostrar();
+
